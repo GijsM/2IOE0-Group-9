3ac79f7507d77510bfa718b0d924af4dce6a06d6
arne <arnegerits@gmail.com>
2020-05-22 17:35:24 +0200

    added map and room as classes, some methods for them too

--- 2IOE0-Group-9/src/app/Util/WindowManager.java
+++ 2IOE0-Group-9/src/app/Util/WindowManager.java
@@ -1,5 +1,6 @@
 package app.Util;
 
+import app.Game.Object.GameMap;
 import org.lwjgl.*;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
@@ -8,6 +9,7 @@ import org.lwjgl.system.*;
 import java.nio.*;
 import java.util.ArrayList;
 
+import static java.lang.StrictMath.round;
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
@@ -18,14 +20,13 @@ public class WindowManager implements IStartStopable{
     private long window;
     private Thread thread;
     //COPIED FROM TUTORIAL NOT SURE IF THIS IS CORRECT
-	ArrayList<ArrayList> map = new ArrayList<>();
+	GameMap map  = new GameMap();
 
     @Override
     public void start() {
 		// Setup an error callback. The default implementation
 		// will print the error message in System.err.
 		GLFWErrorCallback.createPrint(System.err).set();
-		fillMap();
 
 		// Initialize GLFW. Most GLFW functions will not work before doing this.
 		if ( !glfwInit() )
@@ -77,20 +78,6 @@ public class WindowManager implements IStartStopable{
 		glfwShowWindow(window);
 	}
 
-	private void fillMap() {
-    	for(int i = 0; i < 10 ; i ++){
-    		map.add(new ArrayList<Integer>());
-		}
-    	for(int i = 0 ; i < 10 ; i++){
-    		for(int j = 0; j < 10 ; j++){
-    			if(i == 0 || i == 9 || j == 0 || j == 9){
-    				map.get(i).add(0);
-				} else {
-    				map.get(i).add(1);
-				}
-			}
-		}
-	}
 
 	@Override
     public void stop() {
@@ -127,59 +114,11 @@ public class WindowManager implements IStartStopable{
 
 		// Set the clear color
 		glClearColor(0f, 0.0f, 0.0f, 0.0f);
-
 		// Run the rendering loop until the user has attempted to close
 		// the window or has pressed the ESCAPE key.
 		while ( !glfwWindowShouldClose(window) ) {
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-			glColor4d(1f, 0f, 0f, 0.3f);
 
-			glLoadIdentity();
-			float topX =(float) -1;
-			float botX =(float) -0.8;
-			float topY = 1;
-			float botY = (float) 0.8;
-			float delta = (float) 2/map.size();
-			for(float i = 0 ; i < map.size(); i++){
-				for( float j = 0 ; j < map.size(); j++){
-
-					float xOne = topX + j*delta;
-					float xTwo = botX + j*delta;
-					float yOne = topY - i*delta;
-					float yTwo = botY - i*delta;
-
-					if(i == 0 || i == 9 || j == 0 || j == 9){
-
-						glBegin(GL_QUADS);
-
-						glNormal3f(0, 0, 1);
-						glColor3f(0,1,0);
-						glVertex3f(xOne ,yOne, 0); //Draw Back Wall - Top Left
-						glColor3f(0,0,1);
-						glVertex3f(xTwo,yOne, 0); //Draw Back Wall - Top Right
-						glColor3f(0,1,0);
-						glVertex3f(xTwo,yTwo, 0); //Draw Back Wall - Bottom Right
-						glColor3f(0,0,1);
-						glVertex3f(xOne,yTwo, 0); //Draw Back Wall - Bottom Left
-						glEnd();
-					} else {
-						glBegin(GL_QUADS);
-
-						glNormal3f(0, 0, 1);
-						glColor3f(1,0,0);
-						glVertex3f(xOne ,yOne, 0); //Draw Back Wall - Top Left
-						glColor3f(0,0,1);
-						glVertex3f(xTwo,yOne, 0); //Draw Back Wall - Top Right
-						glColor3f(1,0,0);
-						glVertex3f(xTwo,yTwo, 0); //Draw Back Wall - Bottom Right
-						glColor3f(0,0,1);
-						glVertex3f(xOne,yTwo, 0); //Draw Back Wall - Bottom Left
-						glEnd();
-					}
-				}
-			}
-
-
+			this.map.rooms.get(0).render();
 
 
 			glfwSwapBuffers(window); // swap the color buffers
