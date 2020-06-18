5f76fb9dcfd040c84b3cacf374b935f685a845ae
Irisnadine <47541448+Irisnadine@users.noreply.github.com>
2020-06-18 11:32:28 +0200

    Update Vector3f.java
    with methods add() and normalise()

--- 2IOE0-Group-9/src/app/Util/Vector3f.java
+++ 2IOE0-Group-9/src/app/Util/Vector3f.java
@@ -1,6 +1,10 @@
 package app.Util;
 
-public class Vector3f {
+import java.io.Serializable;
+import java.nio.FloatBuffer;
+
+public class Vector3f extends Vector implements Serializable, ReadableVector3f, WritableVector3f {
+    
     private float x, y, z;
 
     public Vector3f(float x, float y, float z) {
@@ -47,4 +51,40 @@ public class Vector3f {
 
         return (float) hypo;
     }
+    /**
+     * Add a vector to another vector and place the result in a destination
+     * vector.
+     *
+     * @param left The LHS vector
+     * @param right The RHS vector
+     * @param dest The destination vector, or null if a new vector is to be
+     * created
+     * @return the sum of left and right in dest
+     */
+    public static Vector3f add(Vector3f left, Vector3f right, Vector3f dest) {
+        if (dest == null) {
+            return new Vector3f(left.x + right.x, left.y + right.y, left.z + right.z);
+        } else {
+            dest.set(left.x + right.x, left.y + right.y, left.z + right.z);
+            return dest;
+        }
+    }
+    
+    /**
+	 * Normalise this vector and place the result in another vector.
+	 * @param dest The destination vector, or null if a new vector is to be created
+	 * @return the normalised vector
+	 */
+	public Vector3f normalise(Vector3f dest) {
+		float l = length();
+
+		if (dest == null)
+			dest = new Vector3f(x / l, y / l, z / l);
+		else
+			dest.set(x / l, y / l, z / l);
+
+		return dest;
+	}
+
+}
 }
